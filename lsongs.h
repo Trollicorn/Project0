e21449e8b01afa2a025d7412750bea4d6d6f329b
StefanTan18 <stan3@stuy.edu>
2018-09-20 08:50:35 -0400

    added lsongs.h

---
+++ lsongs.h
@@ -0,0 +1,22 @@
+struct song_node{ 
+  char name[100];
+  char artist[100];
+  struct song_node *next;
+};
+
+struct song_node * insert_front(struct song_node *s, char n, char a);
+
+struct song_node * insert_order(struct song_node *s, char n, char a);
+
+void print_songs(struct song_node *s);
+
+struct song_node * search_song(struct song_node *s, char n, char a);
+
+struct song_node * search_artist(struct song_node *s, char a);
+
+struct song_node * rand_song(struct song_node *s);
+
+struct song_node * remove_song(struct song_node *s);
+
+struct song_ndoe * free_list(struct song_node *s);
+
