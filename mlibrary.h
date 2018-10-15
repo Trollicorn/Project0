6e896c5e305efdb101d7af022d8a54b1dbaa89cc
Trollicorn <muddin3@stuy.edu>
2018-10-15 10:58:03 -0400

    mlibrary headers

--- mlibrary.h
+++ mlibrary.h
@@ -1,4 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lsong.h"
+#include "lsongs.h"
+
+struct song_node * table[27];
+
+
+void add_song(struct song_node *s);
+
+struct song_node * search_song(char *name);
+
+struct song_node * search_artist(char *name);
+
+void print_entries(char c);
+
+void print_songs(char *name);
+
+void print_lib();
+
+void shuffle();
+
+void delete_song(char *name);
+
+void clear_lib();
 
