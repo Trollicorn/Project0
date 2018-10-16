7a125cf23a76701cd15a9c846827f8d079f351fe
StefanTan18 <stan3@stuy.edu>
2018-10-16 11:02:49 -0400

    update

--- lsongs.c
+++ lsongs.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lsongs.h"
 
 
-struct song_node * insert_front(struct song_node *s, char n, char a){
+struct song_node * insert_front(struct song_node *s, char *n, char *a){
   struct song_node *temp = malloc(sizeof(struct song_node));
   temp->name = n;
   temp->artist = a;
@@ -11,9 +12,13 @@ struct song_node * insert_front(struct song_node *s, char n, char a){
   return temp;
 }
 
-struct song_node * insert_order(struct song_node *s, char n, char a){
-  struct song_node *current = s;
-  while (current){
-
+struct song_node * insert_order(struct song_node *s, char *n, char *a){
+  struct node *prev = s;
+  struct node *current = s; 
+  while (strcmp(a, current->artist) < 0){
+    prev = current;
+    current = current->next;
   }
+  while (strcmp(n, current->name) < 0)
+    current = current->next;
 }
