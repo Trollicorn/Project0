54292c60afd618a1e804da44381b27e9df891ba0
Trollicorn <muddin3@stuy.edu>
2018-10-16 11:03:21 -0400

    sss

--- mlibrary.c
+++ mlibrary.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lsongs.h"
 #include "mlibrary.h"
+
+void add_song(struct song node *s){
+	int l = 0;
+	while (l < 26){
+		if (s->artist[0] == 'a' + char(l)){
+			
+		}
+	}
+}
