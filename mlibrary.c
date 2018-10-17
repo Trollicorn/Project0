e1ac5e4af1408ec94aba3909d1324ead46536681
Trollicorn <muddin3@stuy.edu>
2018-10-17 11:03:00 -0400

    m'library

--- mlibrary.c
+++ mlibrary.c
@@ -10,4 +10,31 @@ void add_song(struct song node *s){
 			
 		}
 	}
+#include <stdio.h>
+#include <stdlib.h>
+#include "lsongs.h"
+#include "mlibrary.h"
+
+void make_table(){
+        for (int i = 0; i < 27; i++){
+                table[i] = NULL;
+        }
 }
+
+
+
+void add_song(struct song node *s){
+        int index;
+        if (s->artist[0] >= 97 && s->artist[0] <= 122){
+                index = (s->artist[0] - 19) % 26;
+        }else{
+                index = 26;
+        }
+        if (!table[index]){
+                table[index] = insert_front(table[index], s->name, s->artist);
+                return;
+        }
+        table[index] = insert_order(table[index], s->name, s->artist);
+}
+~                                                                                                                                                                       
+~                                                                                      }
