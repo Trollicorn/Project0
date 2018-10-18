a9feeb972d23264ff05e1d37502ffb5f65d41e0e
StefanTan18 <stan3@stuy.edu>
2018-10-18 11:01:48 -0400

    updated driver

---
+++ songdriver.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lsongs.h"
+#include "mlibrary.h"
+
+int main() {
+
+  printf("LINKED LIST TESTS\n");
+  printf("====================================\n");
+  printf("\nTesting print_list:\n");
+  print_lib();
+  printf("\nTesting print_node:\n");
+  printf();
+
+}
